04358b03c5f9929ab59e3fc8d9fbda2599a07dba
geraldkachi <fitzgeraldkachi@gmail.com>
2026-04-13 16:57:48 +0100

    update on the code

--- src/components/Table/Table.tsx
+++ src/components/Table/Table.tsx
@@ -1,6 +1,5 @@
 /* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
-import React from "react";
 import type { ReactNode } from "react";import clsx from "clsx";
 import { 
   HiDotsHorizontal, 
